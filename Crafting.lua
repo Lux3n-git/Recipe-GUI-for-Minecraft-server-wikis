5f43f50320d0e3ea384179887b9d37ba0e071311
TripleLuxen <35500599+TripleLuxen@users.noreply.github.com>
2022-05-11 21:22:35 -0700

    Fixed Indents, Added processing of args
    I made the frame arguments instead of spewing out stuff like for example: {{{example}}} when it never gets a value or never gets used, it doesn't get put into a reworked and cleaned version of the frame arguments table (the ones that are actually getting used)
    it also cleans up whitespace too!!!!!

--- Crafting.lua
+++ Crafting.lua
@@ -1,35 +1,39 @@
---This is the module that creates the minecraft table and displays the item sprites
-
---Files Needed:
---CheatInvisible.png
---MCTooltip.lua Module
---ItemSprite.lua Module
---Crafting Table Template (Template:CraftingTable
+--Credits to 
 
 
 local g = {}
-		function sprite(frame, v, toppx, leftpx, OAValue) --creates a sprite instance 
-			OAValue = OAValue or nil
-			function thesprite(spriteimage, colorname, lorename, lorecolor)
-				colorname = colorname or "White"
-				local OA = ""
-				if spriteimage  == "" then
-					return '<div style="width:32px; height:32px; top:'..toppx ..'px; left:'..leftpx ..'px; position:absolute>[[File:CheatInvisible.png|link=]]</div>'
-				else
-					nickt = {}
-					for nick in mw.text.gsplit(spriteimage,"/") do
-						table.insert(nickt, nick)
-					end
-					local tooltipsprite = --sprite tooltip
-						mw.html.create("div")
-						:addClass("tooltip-contents")
-						:wikitext(frame:expandTemplate{title = "MCTooltip", args = { Color = colorname, content = nickt[1], Colorlore = lorecolor, lore = lorename, Nick = nickt[2]}})
-					local itemsprite = --sprite
-						mw.html.create("div")
-						:addClass("advanced-tooltip")
-						:wikitext(frame:expandTemplate{title = "ItemSprite", args = { Item = nickt[1] }})
-					
-					if OAValue ~= nil and OAValue ~= "{{{OA}}}" then
+	function processargs(frame)
+		local args = {}
+		for k, v in pairs(mw.getCurrentFrame():getParent().args) do
+			v = mw.text.trim( tostring( v ) )
+			if v ~= '' then
+				args[k] = v
+			end
+		end
+		return args
+	end
+	function sprite(frame, v, toppx, leftpx, OAValue) --creates a sprite instance 
+		OAValue = OAValue or nil
+		function thesprite(spriteimage, colorname, lorename, lorecolor)
+			colorname = colorname or "White"
+			local OA = ""
+			if spriteimage  == ""  then
+				return '<div style="width:32px; height:32px; top:'..toppx ..'px; left:'..leftpx ..'px; position:absolute>[[File:CheatInvisible.png|link=]]</div>'
+			else
+				nickt = {}
+				for nick in mw.text.gsplit(spriteimage,"/") do
+					table.insert(nickt, nick)
+				end
+				local tooltipsprite = --sprite tooltip
+					mw.html.create("div")
+					:addClass("tooltip-contents")
+					:wikitext(frame:expandTemplate{title = "MCTooltip", args = { Color = colorname, content = nickt[1], Colorlore = lorecolor, lore = lorename, Nick = nickt[2]}})
+				local itemsprite = --sprite
+					mw.html.create("div")
+					:addClass("advanced-tooltip")
+					:wikitext(frame:expandTemplate{title = "ItemSprite", args = { Item = nickt[1] }})
+				
+				if OAValue ~= nil and OAValue ~= "{{{OA}}}" then
 					local OA =
 					mw.html.create("div")
 					:addClass("item-amount")
@@ -43,31 +47,30 @@ local g = {}
 					}
 					itemsprite:node(OA)
 					theresyouritem = itemsprite:node(tooltipsprite)
-					else
-					theresyouritem = itemsprite:node(tooltipsprite)
-					end
-				end
-			return theresyouritem --i suck at making var names
-			end
-			
-			images = {}
-			for image in mw.text.gsplit( v,";") do --splits all names using the ";" given
-				if image == "" or image == "Blank" or image == " Blank" then
-				table.insert( images, '<span><div class=\"item-slot-box\" style="width:32px; height:32px;>[[File:CheatInvisible.png|link=]]</div></span>')
 				else
-						textformat = {}
-						for split in mw.text.gsplit(image,":") do
-							table.insert(textformat, split)
-						end
-						table.insert( images, '<span>'..tostring(thesprite(textformat[1], textformat[2], textformat[3], textformat[4]))..'</span>' )
+					theresyouritem = itemsprite:node(tooltipsprite)
+				end
+			end
+		return theresyouritem --i suck at making var names
+		end
+		images = {}
+		for image in mw.text.gsplit( v,";") do --splits all names using the ";" given
+			if image == "" or image == "Blank" or image == " Blank" then
+			table.insert( images, '<span><div class=\"item-slot-box\" style="width:32px; height:32px;>[[File:CheatInvisible.png|link=]]</div></span>')
+			else
+					textformat = {}
+					for split in mw.text.gsplit(image,":") do
+						table.insert(textformat, split)
+					end
+					table.insert( images, '<span>'..tostring(thesprite(textformat[1], textformat[2], textformat[3], textformat[4]))..'</span>' )
 			end
 		end
 		images[1] = images[1]:gsub( '<span>', '<span class="animated-active">' )
 
 		return '<div class=\"animated item-slot-box\" style="width:32px; height:32px; top:'..toppx ..'px; left:'..leftpx ..'px; position:absolute>'..table.concat(images)..'</div>' --plan B for variables for the div CSS
-
-		end
+	end
 	function g.crafting(frame)
+	frame.args = processargs(frame)	
 	local OA = --minecraft number next to sprite 
 		mw.html.create("div")
 		:addClass("item-amount")
@@ -112,21 +115,22 @@ local g = {}
 	
 	end
 		--attahes every sprite slot to the html division
-		div2:node(sprite(frame:getParent(), frame.args["A1"] or "", 0, 0))
-		div2:node(sprite(frame:getParent(), frame.args["A2"] or "", 36, 0))
-		div2:node(sprite(frame:getParent(), frame.args["A3"] or "", 72, 0))
-	    div2:node(sprite(frame:getParent(), frame.args["B1"] or "", 0, 36))
-	    div2:node(sprite(frame:getParent(), frame.args["B2"] or "", 36, 36))
-	    div2:node(sprite(frame:getParent(), frame.args["B3"] or "", 72, 36))
-		div2:node(sprite(frame:getParent(), frame.args["C1"] or "", 0, 72))
-		div2:node(sprite(frame:getParent(), frame.args["C2"] or "", 36, 72))
-		div2:node(sprite(frame:getParent(), frame.args["C3"] or "", 72, 72))
-		div2:node(sprite(frame:getParent(), frame.args["Output"] or "", 36, 188, frame.args["OA"]))
-		div1:node(div2)
+	div2:node(sprite(frame:getParent(), frame.args["A1"] or "", 0, 0))
+	div2:node(sprite(frame:getParent(), frame.args["A2"] or "", 36, 0))
+	div2:node(sprite(frame:getParent(), frame.args["A3"] or "", 72, 0))
+    	div2:node(sprite(frame:getParent(), frame.args["B1"] or "", 0, 36))
+    	div2:node(sprite(frame:getParent(), frame.args["B2"] or "", 36, 36))
+    	div2:node(sprite(frame:getParent(), frame.args["B3"] or "", 72, 36))
+	div2:node(sprite(frame:getParent(), frame.args["C1"] or "", 0, 72))
+	div2:node(sprite(frame:getParent(), frame.args["C2"] or "", 36, 72))
+	div2:node(sprite(frame:getParent(), frame.args["C3"] or "", 72, 72))
+	div2:node(sprite(frame:getParent(), frame.args["Output"] or "", 36, 188, frame.args["OA"]))
+	div1:node(div2)
 		
 	return div1 -- told ya
 	end
 	function g.smelting(frame)
+	frame.args = processargs(frame)	
 		local div1 =  --main html division, this is the main object that is sent out while everything else is attached or within it
 			mw.html.create("div")
 			:css{
@@ -159,6 +163,7 @@ local g = {}
 		return div1
 	end
 	function g.smithing(frame)
+	frame.args = processargs(frame)	
 		local div1 =  --main html division, this is the main object that is sent out while everything else is attached or within it
 			mw.html.create("div")
 			:css{
